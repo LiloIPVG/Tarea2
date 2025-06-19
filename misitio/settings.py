e51e504df053178d072d76e7142451ad7189a86a
LiloIPVG <juan.ananias@virginiogomez.cl>
2025-06-18 22:33:54 -0400

    Estilos

--- misitio/settings.py
+++ misitio/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
